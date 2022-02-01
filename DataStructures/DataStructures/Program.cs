6b4d93ab420122b92207136497cb4457fa95ec06
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-01 15:10:02 +0530

    [shabana] added UC1-CreateLinkedList

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Builtin");
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(10);
+            list.AddLast(20);
+            list.AddLast(30);
+            foreach (int element in list)
+            {
+                Console.WriteLine(element);
+            }
+                Console.WriteLine("custom");
+                CustomLinkedList linkedList = new CustomLinkedList();
+                linkedList.AddLast(56);
+                linkedList.AddLast(30);
+                linkedList.AddLast(70);
+             // linkedList.AddFirst(70);
+               // linkedList.AddFirst(30);
+               // linkedList.AddFirst(56);
+              //  linkedList.display();
+                //Console.ReadLine();
+
+                    
+
+            
+        }
+    }
+}
